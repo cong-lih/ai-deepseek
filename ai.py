e39655d0c359b21aea91429d19925e579b5964f2
cong-lih <2237520740@qq.com>
2026-06-02 13:39:56 +0800

    Add files via upload

---
+++ ai.py
@@ -0,0 +1,51 @@
+import streamlit as st
+from openai import OpenAI
+
+# 设置页面的配置项
+st.set_page_config(
+    page_title="AI智能伴侣",
+    page_icon="🤖",
+    layout="wide",
+    initial_sidebar_state="expanded",
+    menu_items={}
+)
+
+# 大标题
+st.title("AI智能伴侣")
+
+# Logo（路径不对可以先注释掉）
+st.logo("资源/屏幕截图 2026-05-31 153220.png")
+
+# 系统提示词
+system_prompt = "你是一名非常可爱的AI助理，你的名字叫小甜甜，请你使用温柔可爱的语气回答用户的问题"
+
+# 初始化客户端
+client = OpenAI(
+    api_key="sk-3cdb8b4ca538412ebfa2be25debc2b03",
+    base_url="https://api.deepseek.com/v1"
+)
+
+# 消息输入框
+prompt = st.chat_input("请输入您要问的问题")
+
+# 只有用户输入了内容，才执行下面的逻辑
+if prompt:
+    # 1. 显示用户消息
+    st.chat_message("user").write(prompt)
+    print("调用AI大模型，提示词: ", prompt)
+
+    # 2. 调用AI大模型
+    response = client.chat.completions.create(
+        model="deepseek-chat",
+        messages=[
+            {"role": "system", "content": system_prompt},
+            {"role": "user", "content": prompt},
+        ],
+        stream=False
+    )
+
+    # 3. 输出大模型返回的结果（控制台+页面）
+    print("<---------- 大模型返回的结果: ", response.choices[0].message.content)
+    with st.chat_message("assistant"):
+        # 用st.text禁用Markdown，避免横线问题
+        st.text(response.choices[0].message.content)
